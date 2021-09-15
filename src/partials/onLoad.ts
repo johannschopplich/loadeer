13702ddad5ad4011bf2722214da064d8fed75d1e
Johann Schopplich <mail@johannschopplich.com>
2021-09-15 09:32:54 +0200

    perf: kick off some bytes

--- src/partials/onLoad.ts
+++ src/partials/onLoad.ts
@@ -2,19 +2,19 @@ import { hasNativeLoadingSupport } from "../utils";
 
 export default (element: HTMLImageElement): void => {
   const { dataset } = element;
-  const { sizes } = dataset;
+  const { src, srcset, sizes } = dataset;
 
   if (hasNativeLoadingSupport && element.loading !== "lazy") {
     element.loading = "lazy";
   }
 
-  if (dataset.src) {
-    element.src = dataset.src;
+  if (src) {
+    element.src = src;
     delete dataset.src;
   }
 
-  if (dataset.srcset) {
-    element.srcset = dataset.srcset;
+  if (srcset) {
+    element.srcset = srcset;
     delete dataset.srcset;
 
     if (sizes) {
